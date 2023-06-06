5c6ea0432d3c01c0d915095dbb840e094693efc3
jairo vence mattias <jairo_vencemattias@hotmail.com>
2023-06-06 01:02:28 -0300

    una prueba mas git status

--- flujos.js
+++ flujos.js
@@ -4,3 +4,9 @@ function flujos(num){
 
     }else {return'usted es menor de edad';}
 }
+
+function nombre(str){
+    if(str=="roberto"){
+        return"usted es mi abuelo";
+    }else {return"usted no es mi abuelo";}
+}
